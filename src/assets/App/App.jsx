32f155c9dd2f820de99792e14d0a8f1d8e48c2da
Willian Barbosa <willianbsilva96@gmail.com>
2025-05-21 16:44:30 -0300

    first grid (buyer bob)

---
+++ src/assets/App/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import BuyBob from './Components/BuyBob.jsx'
+
+function App() {
+
+  return (
+    <>
+      <BuyBob />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/App/Components/BuyBob.jsx
@@ -0,0 +1,21 @@
+import './BuyBob.css'
+
+function BuyBob() {
+
+  return (
+    <>
+      <h2>Hearthstone Battlegrounds!</h2>
+      <div class='gridHs'>
+        <div class='pictureBob board'></div>
+        
+        <div class='gridBob board'></div>
+        
+        <div class='gridUser board'></div>
+
+        <div class='pictureUser board'></div>
+      </div>
+    </>
+  )
+}
+
+export default BuyBob
